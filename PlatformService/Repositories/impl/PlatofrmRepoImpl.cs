c315900b8ceccdc8d8a37e1a65b8e85ee9ca4e7c
mni <muhamadiqbal1987@gmail.com>
2021-12-19 09:43:28 +0700

    Add Models, Repository for Platform

---
+++ PlatformService/Repositories/impl/PlatofrmRepoImpl.cs
@@ -0,0 +1,43 @@
+﻿using PlatformService.Data;
+using PlatformService.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PlatformService.Repositories.impl
+{
+    public class PlatofrmRepoImpl : IPlatformRepo
+    {
+        private readonly AppDbContext appDbContext;
+        public PlatofrmRepoImpl(AppDbContext appDbContext)
+        {
+            this.appDbContext = appDbContext;
+        }
+
+        public void CreatePlatform(Platform plat)
+        {
+            if(plat == null)
+            {
+                throw new ArgumentNullException(nameof(plat));
+            }
+
+            appDbContext.Platforms.Add(plat);
+        }
+
+        public IEnumerable<Platform> GetAllPlatforms()
+        {
+            return appDbContext.Platforms.ToList();
+        }
+
+        public Platform GetPlatformById(int id)
+        {
+            return appDbContext.Platforms.FirstOrDefault(p => p.Id == id);
+        }
+
+        public bool SaveChanges()
+        {
+            return (appDbContext.SaveChanges() > 0);
+        }
+    }
+}
